efb658cb8fa4384a3d3c8952e9e26be46247fec4
Christian T. Jacobs <christian@christianjacobs.uk>
2017-11-29 21:58:02 +0000

    Added cycle crossover functionality and added more comments.

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,4 @@
-""" Solves a Sudoku puzzle using a genetic algorithm. This is based on a piece of coursework produced as part of the CS3M6 Evolutionary Computation module at the University of Reading.
+""" Solves a Sudoku puzzle using a genetic algorithm. This is an improved version of a piece of coursework produced as part of the CS3M6 Evolutionary Computation module at the University of Reading.
 
 @author: Christian Thomas Jacobs
 """
@@ -7,22 +7,18 @@ import numpy
 import random
 random.seed()
 
-Nd = 9
+Nd = 9  # Number of digits (in the case of standard Sudoku puzzles, this is 9).
 
-def sort_fitness(x, y):
-    if(x.fitness < y.fitness):
-        return 1
-    elif(x.fitness == y.fitness):
-        return 0
-    else:
-        return -1
-            
+
 class Population(object):
+    """ A set of candidate solutions to the Sudoku puzzle. These candidates are also known as the chromosomes in the population. """
+
     def __init__(self):
+        self.candidates = []
         return
 
-    def seed(self, Np, given):
-        self.candidates = []  # The candidate solutions (also known as the chromosomes) in the population.
+    def seed(self, Nc, given):
+        self.candidates = []
         
         # Determine the legal values that each square can take.
         helper = Candidate()
@@ -39,7 +35,7 @@ class Population(object):
                         break
 
         # Seed a new population.       
-        for p in range(0, Np):
+        for p in range(0, Nc):
             g = Candidate()
             for i in range(0, Nd): # New row in candidate.
                 row = numpy.zeros(Nd)
@@ -63,26 +59,45 @@ class Population(object):
                 g.values[i] = row
 
             self.candidates.append(g)
-            print(g.values)
-            print(g.fitness)
+        
+        # Compute the fitness of all candidates in the population.
+        self.update_fitness()
         
         print("Seeding complete.")
         
         return
         
+    def update_fitness(self):
+        """ Update fitness of every candidate/chromosome. """
+        for candidate in self.candidates:
+            candidate.update_fitness()
+        return
+        
     def sort(self):
         """ Sort the population based on fitness. """
-        self.candidates.sort(sort_fitness)
+        self.candidates.sort(key=self.sort_fitness)
         return
-    
+
+    def sort_fitness(self, x, y):
+        """ The sorting function. """
+        if(x.fitness < y.fitness):
+            return 1
+        elif(x.fitness == y.fitness):
+            return 0
+        else:
+            return -1
+
 
 class Candidate(object):
+    """ A candidate solutions to the Sudoku puzzle. """
     def __init__(self):
         self.values = numpy.zeros((Nd, Nd))
+        self.fitness = None
         return
 
-    @property
-    def fitness(self):
+    def update_fitness(self):
+        """ The fitness of a candidate solution is determined by how close it is to being the actual solution to the puzzle. The actual solution (i.e. the 'fittest') is defined as a 9x9 grid of numbers in the range [1, 9] where each row, column and 3x3 block contains the numbers [1, 9] without any duplicates (see e.g. http://www.sudoku.com/); if there are any duplicates then the fitness will be lower. """
+        
         column_count = numpy.zeros(Nd)
         block_count = numpy.zeros(Nd)
         column_sum = 0
@@ -118,11 +133,12 @@ class Candidate(object):
             fitness = 1.0
         else:
             fitness = column_sum * block_sum
-            
-        return fitness
+        
+        self.fitness = fitness
+        return
         
     def mutate(self, mutation_rate, given):
-        """ Mutate a candidate. """
+        """ Mutate a candidate by picking a row, and then picking two values within that row to swap. """
 
         r = random.uniform(0, 1.1)
         while(r > 1): # Outside [0, 1] boundary - choose another
@@ -157,27 +173,30 @@ class Candidate(object):
     
         return success
 
-class GivenGrid(Candidate):
+
+class Given(Candidate):
+    """ The grid containing the given/known values. """
+
     def __init__(self, values):
         self.values = values
         return
         
     def is_row_duplicate(self, row, value):
-        """ Check whether there is a duplicate of a FIXED value in a row. """
+        """ Check whether there is a duplicate of a fixed/given value in a row. """
         for column in range(0, Nd):
             if(self.values[row][column] == value):
                return True
         return False
 
     def is_column_duplicate(self, column, value):
-        """ Check whether there is a duplicate of a FIXED value in a column. """
+        """ Check whether there is a duplicate of a fixed/given value in a column. """
         for row in range(0, Nd):
             if(self.values[row][column] == value):
                return True
         return False
 
     def is_block_duplicate(self, row, column, value):
-        """ Check whether there is a duplicate of a FIXED value in a 3 x 3 block. """
+        """ Check whether there is a duplicate of a fixed/given value in a 3 x 3 block. """
         i = 3*(int(row/3))
         j = 3*(int(column/3))
 
@@ -194,7 +213,12 @@ class GivenGrid(Candidate):
         else:
             return False
 
+
 class Tournament(object):
+    """ The crossover function requires two parents to be selected from the population pool. The Tournament class is used to do this.
+    
+    Two individuals are selected from the population pool and a random number in [0, 1] is chosen. If this number is less than the 'selection rate' (e.g. 0.85), then the fitter individual is selected; otherwise, the weaker one is selected.
+    """
 
     def __init__(self):
         return
@@ -216,14 +240,109 @@ class Tournament(object):
 
         selection_rate = 0.85
         r = random.uniform(0, 1.1)
-        while(r > 1): # Outside [0, 1] boundary. Choose another.
+        while(r > 1):  # Outside [0, 1] boundary. Choose another.
             r = random.uniform(0, 1.1)
         if(r < selection_rate):
             return fittest
         else:
             return weakest
+    
+class CycleCrossover(object):
+    """ Crossover relates to the analogy of genes within each parent candidate mixing together in the hopes of creating a fitter child candidate. Cycle crossover is used here (see e.g. A. E. Eiben, J. E. Smith. Introduction to Evolutionary Computing. Springer, 2007). """
+
+    def __init__(self):
+        return
+    
+    def crossover(self, parent1, parent2, crossover_rate):
+        """ Create two new child candidates by crossing over parent genes. """
+        child1 = Candidate()
+        child2 = Candidate()
         
+        # Make a copy of the parent genes.
+        child1.values = numpy.copy(parent1.values)
+        child2.values = numpy.copy(parent2.values)
+
+        r = random.uniform(0, 1.1)
+        while(r > 1):  # Outside [0, 1] boundary. Choose another.
+            r = random.uniform(0, 1.1)
+            
+        # Perform crossover.
+        if (r < crossover_rate):
+            # Pick a crossover point. Crossover must have at least 1 row (and at most Nd-1) rows.
+            crossover_point1 = random.randint(0, 8)
+            crossover_point2 = random.randint(1, 9)
+            while(crossover_point1 == crossover_point2):
+                crossover_point1 = random.randint(0, 8)
+                crossover_point2 = random.randint(1, 9)
+                
+            if(crossover_point1 > crossover_point2):
+                temp = crossover_point1
+                crossover_point1 = crossover_point2
+                crossover_point2 = temp
+                
+            for i in range(crossover_point1, crossover_point2):
+                child1.values[i], child2.values[i] = self.crossover_rows(child1.values[i], child2.values[i])
+
+        return child1, child2
+
+    def crossover_rows(self, row1, row2): 
+        child_row1 = numpy.zeros(Nd)
+        child_row2 = numpy.zeros(Nd)
+
+        remaining = range(1, Nd+1)
+        cycle = 0
+        
+        while((0 in child_row1) and (0 in child_row2)):  # While child rows not complete...
+            if(cycle % 2 == 0):  # Even cycles.
+                # Assign next unused value.
+                index = self.find_unused(row1, remaining)
+                start = row1[index]
+                remaining.remove(row1[index])
+                child_row1[index] = row1[index]
+                child_row2[index] = row2[index]
+                next = row2[index]
+                
+                while(next != start):  # While cycle not done...
+                    index = self.find_value(row1, next)
+                    child_row1[index] = row1[index]
+                    remaining.remove(row1[index])
+                    child_row2[index] = row2[index]
+                    next = row2[index]
+
+                cycle += 1
+
+            else:  # Odd cycle - flip values.
+                index = self.find_unused(row1, remaining)
+                start = row1[index]
+                remaining.remove(row1[index])
+                child_row1[index] = row2[index]
+                child_row2[index] = row1[index]
+                next = row2[index]
+                
+                while(next != start):  # While cycle not done...
+                    index = self.find_value(row1, next)
+                    child_row1[index] = row2[index]
+                    remaining.remove(row1[index])
+                    child_row2[index] = row1[index]
+                    next = row2[index]
+                    
+                cycle += 1
+            
+        return child_row1, child_row2  
+           
+    def find_unused(self, parent_row, remaining):
+        for i in range(0, len(parent_row)):
+            if(parent_row[i] in remaining):
+                return i
+
+    def find_value(self, parent_row, value):
+        for i in range(0, len(parent_row)):
+            if(parent_row[i] == value):
+                return i
+            
 class Sudoku(object):
+    """ Solves a given Sudoku puzzle using a genetic algorithm. """
+
     def __init__(self):
         self.given = None
         return
@@ -232,13 +351,19 @@ class Sudoku(object):
         # Load a configuration to solve.
         with open(path, "r") as f:
             values = numpy.loadtxt(f).reshape((Nd, Nd)).astype(int)
-            self.given = GivenGrid(values)
+            self.given = Given(values)
         return
-    
+
+    def save(self, path, solution):
+        # Save a configuration to a file.
+        with open(path, "w") as f:
+            numpy.savetxt(f, solution.values.reshape(Nd*Nd), fmt='%d')
+        return
+        
     def solve(self):
-        Np = 10  # Population size.
-        Ne = 2  # Number of elites.
-        Ng = 2  # Number of generations.
+        Nc = 100  # Number of candidates (i.e. population size).
+        Ne = int(0.05*Nc)  # Number of elites.
+        Ng = 1000  # Number of generations.
         Nm = 0  # Number of mutations.
         
         # Mutation parameters.
@@ -248,22 +373,25 @@ class Sudoku(object):
     
         # Create an initial population.
         self.population = Population()
-        self.population.seed(Np, self.given)
+        self.population.seed(Nc, self.given)
     
         # For up to 10000 generations...
         stale = 0
         for generation in range(0, Ng):
+        
+            print("Generation %d" % generation)
             
             # Check for a solution.
-            for c in range(0, Np):
+            for c in range(0, Nc):
                 if(self.population.candidates[c].fitness == 1):
                     print("Solution found at generation %d!" % generation)
-                    print(self.population.candidates[c])
+                    print(self.population.candidates[c].values)
                     return self.population.candidates[c]
 
+            # Create the next population.
             next_population = []
 
-            # Select elites.
+            # Select elites (the fittest candidates) and preserve them for the next generation.
             self.population.sort()
             elites = []
             for e in range(0, Ne):
@@ -271,48 +399,48 @@ class Sudoku(object):
                 elite.values = numpy.copy(self.population.candidates[e].values)
                 elites.append(elite)
 
-            # If no solution present, create the next population.
-            for count in range(Ne, Np, 2):
+            # Create the rest of the candidates.
+            for count in range(Ne, Nc, 2):
                 # Select parents from population via a tournament.
                 t = Tournament()
                 parent1 = t.compete(self.population.candidates)
                 parent2 = t.compete(self.population.candidates)
                 
                 ## Cross-over.
-                #child1, child2 = CycleCrossover(parent1, parent2)
-                import copy
-                child1 = copy.deepcopy(parent1)
-                child2 = copy.deepcopy(parent2)
+                cc = CycleCrossover()
+                child1, child2 = cc.crossover(parent1, parent2, crossover_rate=1.0)
                 
                 # Mutate child1.
                 old_fitness = child1.fitness
                 success = child1.mutate(mutation_rate, self.given)
+                child1.update_fitness()
                 if(success):
                     Nm += 1
-                    if(child1.fitness > old_fitness):  # Used to calculate the relative success rate of mutations
+                    if(child1.fitness > old_fitness):  # Used to calculate the relative success rate of mutations.
                         phi = phi + 1
                 
                 # Mutate child2.
                 old_fitness = child2.fitness
                 success = child2.mutate(mutation_rate, self.given)
+                child2.update_fitness()
                 if(success):
                     Nm += 1
-                    if(child2.fitness > old_fitness):  # Used to calculate the relative success rate of mutations
+                    if(child2.fitness > old_fitness):  # Used to calculate the relative success rate of mutations.
                         phi = phi + 1
                 
-                ## Add children to new population
+                # Add children to new population.
                 next_population.append(child1)
                 next_population.append(child2)
 
             # Append elites onto the end of the population. These will not have been affected by crossover or mutation.
             for e in range(0, Ne):
                 next_population.append(elites[e])
-            
-            
+                
             # Select next generation.
             self.population.candidates = next_population
+            self.population.update_fitness()
             
-            # Calculate new adaptive mutation rate (based on Rechenberg's 1/5 success rule).
+            # Calculate new adaptive mutation rate (based on Rechenberg's 1/5 success rule). This is to stop too much mutation as the fitness progresses towards unity.
             if(Nm == 0):
                 phi = 0  # Avoid divide by zero.
             else:
@@ -332,17 +460,21 @@ class Sudoku(object):
             else:
                 stale += 1
 
-            # Kill off the population if 100 generations have passed without a change in their best fitness.
+            # Re-seed the population if 100 generations have passed with the fittest two candidates always having the same fitness.
             if(stale >= 100):
-                print "Earth Quake!"
-                self.population.seed(Np, self.given)
+                print("The population has gone stale. Re-seeding...")
+                self.population.seed(Nc, self.given)
                 stale = 0
                 sigma = 1
                 phi = 0
                 mutations = 0
                 mutation_rate = 0.06
         
+        print("No solution found.")
+        return None
         
 s = Sudoku()
-s.load("puzzle.txt")
-s.solve()
+s.load("puzzle_mild.txt")
+solution = s.solve()
+if(solution):
+    s.save("solution.txt", solution)
